9a2a0a9b18df050e20fcf1ae8cd9f195fa5e757f
Bartlomiej Gmerek <bartlomiej.gmerek@canonical.com>
2024-02-12 10:09:01 +0100

    Updating the Terraform module

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,27 +1,15 @@
+# Copyright 2024 Canonical Ltd.
+# See LICENSE file for licensing details.
+
 resource "juju_application" "gnbsim" {
-  name  = "gnbsim"
+  name  = var.app_name
   model = var.model_name
 
   charm {
     name    = "sdcore-gnbsim-k8s"
     channel = var.channel
   }
-  config = var.gnb-config
+  config = var.config
   units  = 1
   trust  = true
 }
-
-resource "juju_integration" "gnbsim-amf" {
-  model = var.model_name
-
-  application {
-    name     = juju_application.gnbsim.name
-    endpoint = "fiveg-n2"
-  }
-
-  application {
-    name     = var.amf_application_name
-    endpoint = "fiveg-n2"
-  }
-}
-
