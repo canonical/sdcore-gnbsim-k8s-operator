20a3695524fd1b12fc8917082011950121ef0b66
Patricia Reinoso <patricia.reinoso@canonical.com>
2024-05-03 12:03:58 +0000

    restore test class

--- tests/unit/lib/charms/sdcore_gnbsim/v0/test_fiveg_gnb_indentity_requirer.py
+++ tests/unit/lib/charms/sdcore_gnbsim/v0/test_fiveg_gnb_indentity_requirer.py
@@ -1,30 +1,42 @@
 # Copyright 2023 Canonical Ltd.
 # See LICENSE file for licensing details.
 
-import unittest
 from unittest.mock import call, patch
 
-from ops import testing
+import pytest
+from ops import BoundEvent, testing
 from test_charms.test_requirer_charm.src.charm import WhateverCharm  # type: ignore[import]
 
 TEST_CHARM_PATH = "charms.sdcore_gnbsim_k8s.v0.fiveg_gnb_identity.GnbIdentityRequirerCharmEvents"
+RELATION_NAME = "fiveg_gnb_identity"
 
+class TestGnbIdentityRequires:
 
-class TestGnbIdentityRequires(unittest.TestCase):
+    patcher_gnb_identity = patch(f"{TEST_CHARM_PATH}.fiveg_gnb_identity_available")
+
+    @pytest.fixture()
     def setUp(self) -> None:
-        self.harness = testing.Harness(WhateverCharm)
-        self.addCleanup(self.harness.cleanup)
-        self.harness.begin()
-        self.relation_name = "fiveg_gnb_identity"
+        self.mock_gnb_identity = TestGnbIdentityRequires.patcher_gnb_identity.start()
+        self.mock_gnb_identity.__class__ = BoundEvent
 
-    @patch(f"{TEST_CHARM_PATH}.fiveg_gnb_identity_available")
+    def tearDown(self) -> None:
+        patch.stopall()
+
+    @pytest.fixture(autouse=True)
+    def harness(self, setUp, request):
+        self.harness = testing.Harness(WhateverCharm)
+        self.harness.begin()
+        yield self.harness
+        self.harness.cleanup()
+        request.addfinalizer(self.tearDown)
+
     def test_given_relation_with_gnb_identity_provider_when_gnb_identity_available_event_then_gnb_identity_information_is_provided(  # noqa: E501
-        self, patched_gnb_identity_available_event
+        self,
     ):
         test_gnb_name = "gnb0055"
         test_tac = 1234
         relation_id = self.harness.add_relation(
-            relation_name=self.relation_name, remote_app="whatever-app"
+            relation_name=RELATION_NAME, remote_app="whatever-app"
         )
         self.harness.add_relation_unit(relation_id, "whatever-app/0")
 
@@ -37,4 +49,4 @@ class TestGnbIdentityRequires(unittest.TestCase):
         calls = [
             call.emit(gnb_name=test_gnb_name, tac=str(test_tac)),
         ]
-        patched_gnb_identity_available_event.assert_has_calls(calls)
+        self.mock_gnb_identity.assert_has_calls(calls)
